3e4fe255c499b8f3f4caa9fced3a328b2ecdca0e
Diego <diegosirtoli@hotmail.com>
2025-03-28 16:39:34 -0300

    atualização

--- gruntfile.js
+++ gruntfile.js
@@ -1,58 +1,47 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         less: {
-            dev: {
-                files: {
-                    'dev/styles/main.css': 'src/styles/main.less'
-                }
-            },
             dist: {
-                options: {
-                    compress: true
-                },
+                options: { compress: true },
                 files: {
-                    'dist/styles/main.min.css': 'src/styles/main.less'
-                }
-            }
+                    "dist/styles/main.css": "src/styles/main.less",
+                },
+            },
         },
         uglify: {
             dist: {
                 files: {
-                    'dist/scripts/main.min.js': ['src/scripts/main.js']
-                }
-            }
+                    "dist/scripts/main.js": ["src/scripts/main.js"],
+                },
+            },
         },
         copy: {
             html: {
                 files: [
-                    { src: 'src/index.html', dest: 'dist/index.html' },
-                    { src: 'src/index.html', dest: 'dev/index.html' }
-                ]
-            }
+                    { src: "src/index.html", dest: "dist/index.html" },
+                ],
+            },
         },
         watch: {
-            scripts: {
-                files: ['src/scripts/**/*.js'],
-                tasks: ['uglify'],
-                options: { spawn: false }
-            },
             styles: {
-                files: ['src/styles/**/*.less'],
-                tasks: ['less'],
-                options: { spawn: false }
+                files: ["src/styles/**/*.less"],
+                tasks: ["less"],
+            },
+            scripts: {
+                files: ["src/scripts/**/*.js"],
+                tasks: ["uglify"],
             },
             html: {
-                files: ['src/**/*.html'],
-                tasks: ['copy'],
-                options: { spawn: false }
-            }
-        }
+                files: ["src/*.html"],
+                tasks: ["copy"],
+            },
+        },
     });
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
-    grunt.registerTask('default', ['less', 'uglify', 'copy']);
+    grunt.registerTask("default", ["less", "uglify", "copy"]);
 };
